6a2392168a15f844055c3ec71a6205f8868616ee
ellochen <silver.accc@gmail.com>
2013-05-02 08:33:15 +0800

    add lex file

---
+++ main.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: main.c
+	> Author: ellochen
+	> Mail: god_mode@yeah.net
+	> Created Time: Mon Apr 29 21:36:12 2013
+ ************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	
+	return 0;
+}
